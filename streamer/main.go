3db8e396591557ef58cee6b5dd0811e0a9df66a1
John Lonergan <john.lonergan@gmail.com>
2022-09-06 09:54:04 +0100

    clean up

--- streamer/main.go
+++ streamer/main.go
@@ -21,7 +21,6 @@ package main
 */
 
 import (
-	"crypto/md5"
 	"flag"
 	"fmt"
 	"log"
@@ -62,10 +61,6 @@ func main() {
 		StreamCmd()
 	case "info":
 		InfoCmd()
-	case "md5":
-		Md5Cmd()
-	case "update":
-		UpdateCmd()
 	default:
 		Usage()
 		os.Exit(exitCodeCmdParsing)
@@ -81,9 +76,11 @@ func Usage() {
 	fmt.Println("")
 
 	fmt.Println("Available commands:")
-	fmt.Println("  - info <file.ym>             : Get metadata of a YM file")
-	fmt.Println("  - md5 <file.ym>              : Compute the MD5 sum of a YM song data")
-	fmt.Println("  - update <options> <file.ym> : Update YM file metadata")
+	fmt.Println("  stream <file.ym>           : Stream YM file over the USB/Comm port")
+	fmt.Println("  info <file.ym>             : Get metadata of a YM file")
+	fmt.Println("")
+	flag.PrintDefaults()
+
 	fmt.Println("")
 	fmt.Println("Run a command without argument to get more information.")
 	fmt.Println("")
@@ -92,6 +89,7 @@ func Usage() {
 	for version := range ym.SupportedYmVersions {
 		fmt.Printf("%v ", version)
 	}
+
 	fmt.Println("")
 	fmt.Println("")
 
@@ -104,7 +102,6 @@ func Usage() {
 	fmt.Println("WARNING: This tool doesn't unpack LHA/LZH compressed YM files.")
 	fmt.Println("The YM file is expected to be already unpacked.")
 
-	flag.PrintDefaults()
 }
 
 func StreamCmd() {
@@ -249,108 +246,6 @@ func InfoCmd() {
 	fmt.Printf("Information for %v:\n\n", filePath)
 	fmt.Println(ymFile.Header)
 }
-
-// Md5Cmd computes the MD5 hash of a YM file
-func Md5Cmd() {
-	if len(flag.Args()) < 2 {
-		fmt.Println("YM Tool - Compute MD5 hash of a song data")
-		fmt.Println("")
-		fmt.Println("  This will compute the MD5 hash of a song data (i.e. the YM registers")
-		fmt.Println("  ignoring the song metadata. Useful to find duplicate songs that have")
-		fmt.Println("  different metadata")
-		fmt.Println("")
-		fmt.Println("Usage: ymtool md5 <file.ym>")
-
-		os.Exit(exitCodeCmdParsing)
-	}
-
-	filePath := flag.Arg(1)
-	ymFile, err := ym.NewFile(filePath, false)
-	switch err := err.(type) {
-	case nil:
-	case ym.UnsupportedVersionError:
-		fmt.Printf("Error opening file %v: %v\n", filePath, err)
-		os.Exit(exitCodeUnsupportedVersion)
-	default:
-		fmt.Printf("Error opening file %v: %v\n", filePath, err)
-		os.Exit(exitCodeIOError)
-	}
-
-	fmt.Printf("%x\t%v\n", md5.Sum(ymFile.Frames), filePath)
-}
-
-// UpdateCmd updates the metadata of a YM file
-func UpdateCmd() {
-
-	fs := flag.NewFlagSet("Update options", flag.ExitOnError)
-	songName := fs.String("song-name", "", "Name of the song")
-	author := fs.String("author", "", "Song author")
-	comment := fs.String("comment", "", "Song comment")
-
-	err := fs.Parse(os.Args[2:])
-	if err != nil {
-		fmt.Printf("Error parsing command line flags: %v", err)
-		os.Exit(exitCodeCmdParsing)
-	}
-
-	if len(fs.Args()) < 1 || (*songName == "" && *author == "" && *comment == "") {
-		fmt.Println("YM Tool - Update metadata of a song")
-		fmt.Println("")
-		fmt.Println("Usage: ymtool update [options...] <file.ym>")
-		fmt.Println("")
-		fmt.Println("Options:")
-		fs.PrintDefaults()
-		fmt.Println("")
-		fmt.Println("Example:")
-		fmt.Println("  ymtool update -author=Joe -song-name=\"Joe's song\" file.ym")
-
-		os.Exit(exitCodeCmdParsing)
-	}
-
-	filePath := fs.Arg(0)
-	ymFile, err := ym.NewFile(filePath, false)
-	if err != nil {
-		fmt.Printf("Error opening file %v: %v\n", filePath, err)
-		os.Exit(exitCodeIOError)
-	}
-
-	updated := false
-	if *songName != "" {
-		ymFile.Header.SongName = *songName
-		updated = true
-	}
-	if *author != "" {
-		ymFile.Header.Author = *author
-		updated = true
-	}
-	if *comment != "" {
-		ymFile.Header.Comment = *comment
-		updated = true
-	}
-
-	if updated {
-		data, err := ymFile.MarshalBinary()
-		if err != nil {
-			fmt.Printf("Error marshalling updated file to binary: %v", err)
-			os.Exit(exitCodeIOError)
-		}
-
-		f, err := os.Create(filePath)
-		if err != nil {
-			fmt.Printf("Error opening output file %v for writing: %v", filePath, err)
-			os.Exit(exitCodeIOError)
-		}
-		defer f.Close()
-
-		_, err = f.Write(data)
-		if err != nil {
-			fmt.Printf("Error writing output file: %v", err)
-			os.Exit(exitCodeIOError)
-		}
-
-	}
-}
-
 
 func openCom() (*serial.Port) {
 
